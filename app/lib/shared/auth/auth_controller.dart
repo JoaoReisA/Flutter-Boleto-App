7f8cf439a52a4e283f546dd0e0ab6599e3f0afce
João Victor Reis Alves <joao.reisa13@gmail.com>
2021-11-17 09:30:45 -0300

    Creating a UserModel with methods to sustent the SharedPreferences

--- app/lib/shared/auth/auth_controller.dart
+++ app/lib/shared/auth/auth_controller.dart
@@ -1,15 +1,17 @@
 
 import 'package:app/modules/home/home_page.dart';
 import 'package:app/modules/login/login_page.dart';
+import 'package:app/shared/models/user_model.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
   var _isAuthenticated = false;
-  var _user;
+  UserModel? _user;
 
-  get user => _user;
+  UserModel get user => _user!;
 
-  void setUser(var user, BuildContext context) {
+  void setUser(UserModel? user, BuildContext context) {
     if (user != null) {
       _user = user;
       _isAuthenticated = true;
@@ -20,5 +22,18 @@ class AuthController {
             Navigator.pushReplacementNamed(
           context,"/login");
     }
+  }
+
+  Future<void> saveUser(UserModel user) async{
+    final instance = await SharedPreferences.getInstance();
+    await instance.setString("user", user.toJson());
+    return;
+  }
+
+ Future<void> currentUser(BuildContext context) async {
+    final instance = await SharedPreferences.getInstance();
+    final json = instance.get("user") as String;
+    setUser(UserModel.fromJson(json), context);
+    return;
   }
 }
